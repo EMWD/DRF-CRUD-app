9f26dfa9fee48c959ca5e38ce9973fcdc09e29ba
EMWD <imba2511@yandex.ru>
2021-08-21 21:51:51 +0300

    second

---
+++ drf_proj/drf_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(blank=True)
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=120, blank=True)
+    description = models.TextField(blank=True)
+    body = models.TextField(blank=True)
+    author = models.ForeignKey('Author', related_name='articles', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
