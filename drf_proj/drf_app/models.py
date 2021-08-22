d1936693751636b15b29177e17926aa688fce12f
EMWD <imba2511@yandex.ru>
2021-08-22 20:52:32 +0300

    put delete methods added

--- drf_proj/drf_app/models.py
+++ drf_proj/drf_app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=255, blank=True)
     email = models.EmailField(blank=True)
+    
     def __str__(self):
         return self.name
 
@@ -12,5 +13,6 @@ class Article(models.Model):
     description = models.TextField(blank=True)
     body = models.TextField(blank=True)
     author = models.ForeignKey('Author', related_name='articles', on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
